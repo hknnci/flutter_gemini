a80e04da7265cb117730bb1f66e0ee77c4e295b3
hknnci <hakannaci07@gmail.com>
2024-05-05 01:25:49 +0300

    ChatScreen widget has been updated to include a GestureDetector to dismiss the keyboard when tapping outside the TextField. The `InputField` widget has also that feature now

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -6,72 +6,75 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
 
 class ChatScreen extends StatelessWidget {
-  final TextEditingController _controller = TextEditingController();
-
-  ChatScreen({super.key});
+  const ChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     final chatbotProvider = Provider.of<ChatbotProvider>(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text.rich(
-          TextSpan(
-            children: <InlineSpan>[
-              TextSpan(
-                text: 'Gemini ',
-                style: TextStyle(
-                  foreground: Paint()
-                    ..shader = const LinearGradient(
-                      colors: <Color>[Colors.purple, Colors.blue],
-                    ).createShader(const Rect.fromLTWH(0.0, 0.0, 200.0, 70.0)),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text.rich(
+            TextSpan(
+              children: <InlineSpan>[
+                TextSpan(
+                  text: 'Gemini ',
+                  style: TextStyle(
+                    foreground: Paint()
+                      ..shader = const LinearGradient(
+                        colors: <Color>[Colors.purple, Colors.blue],
+                      ).createShader(
+                        const Rect.fromLTWH(0.0, 0.0, 200.0, 70.0),
+                      ),
+                  ),
                 ),
-              ),
-              const TextSpan(
-                text: 'Chatbot App',
-              ),
-            ],
+                const TextSpan(
+                  text: 'Chatbot App',
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-      body: Column(
-        children: <Widget>[
-          NotificationListener<ScrollMetricsNotification>(
-            onNotification: (notification) {
-              if (notification.metrics.extentAfter > 12) {
-                chatbotProvider.animateScrollController();
-              }
-              return true;
-            },
-            child: Expanded(
-              child: SingleChildScrollView(
-                controller: chatbotProvider.scrollController,
-                child: Consumer<ChatbotProvider>(
-                  builder: (context, chat, _) => Stack(
-                    children: [
-                      ListView.builder(
-                        itemCount: chat.messages.length,
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemBuilder: (context, index) => ChatMessageTile(
-                          message: chat.messages[index],
+        body: Column(
+          children: <Widget>[
+            NotificationListener<ScrollMetricsNotification>(
+              onNotification: (notification) {
+                if (notification.metrics.extentAfter > 12) {
+                  chatbotProvider.animateScrollController();
+                }
+                return true;
+              },
+              child: Expanded(
+                child: SingleChildScrollView(
+                  controller: chatbotProvider.scrollController,
+                  child: Consumer<ChatbotProvider>(
+                    builder: (context, chat, _) => Stack(
+                      children: [
+                        ListView.builder(
+                          itemCount: chat.messages.length,
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemBuilder: (context, index) => ChatMessageTile(
+                            message: chat.messages[index],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          if (chatbotProvider.sendingMessage)
-            const Center(
-              child: SpinKitThreeBounce(color: Colors.white, size: 20),
-            ),
-          const SizedBox(height: 2),
-          InputField(controller: _controller),
-        ],
+            if (chatbotProvider.sendingMessage)
+              const Center(
+                child: SpinKitThreeBounce(color: Colors.white, size: 20),
+              ),
+            const SizedBox(height: 2),
+            const InputField(),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/input_field.dart
+++ lib/widgets/input_field.dart
@@ -4,75 +4,78 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
 class InputField extends StatelessWidget {
-  final TextEditingController controller;
-
-  const InputField({super.key, required this.controller});
+  const InputField({super.key});
 
   @override
-  Widget build(BuildContext context) => Container(
-        decoration: const BoxDecoration(
-          color: Color(0xff111318),
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 5.0,
-              color: Color(0xff111318),
-              offset: Offset(3.0, 0),
+  Widget build(BuildContext context) {
+    final TextEditingController controller = TextEditingController();
+
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color(0xff111318),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 5.0,
+            color: Color(0xff111318),
+            offset: Offset(3.0, 0),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: const Color(0xFF2b2b2b),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 12.0,
+                          vertical: 4.0,
+                        ),
+                        child: TextField(
+                          controller: controller,
+                          decoration: const InputDecoration(
+                            hintText: 'Enter your message',
+                            border: InputBorder.none,
+                          ),
+                          onSubmitted: (text) {
+                            _sendMessage(context, controller.text);
+                            controller.clear();
+                          },
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        _sendMessage(context, controller.text);
+                        FocusScope.of(context).unfocus();
+                        controller.clear();
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SvgPicture.asset(
+                          'assets/gemini-icon.svg',
+                          height: 32,
+                          width: 32,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.0),
-                    color: const Color(0xFF2b2b2b),
-                  ),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 12.0,
-                            vertical: 4.0,
-                          ),
-                          child: TextField(
-                            controller: controller,
-                            decoration: const InputDecoration(
-                              hintText: 'Enter your message',
-                              border: InputBorder.none,
-                            ),
-                            onSubmitted: (text) {
-                              _sendMessage(context, controller.text);
-                              controller.clear();
-                            },
-                          ),
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          _sendMessage(context, controller.text);
-                          controller.clear();
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: SvgPicture.asset(
-                            'assets/gemini-icon.svg',
-                            height: 32,
-                            width: 32,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      );
+      ),
+    );
+  }
 
   void _sendMessage(BuildContext context, String message) {
     if (message.isNotEmpty) {
